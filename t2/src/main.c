fee534bdfd5d7850f8ae8d38fff66c4ba28aa83c
sean <1132119621@qq.com>
2018-06-12 18:36:53 +0800

    exercise and book

---
+++ t2/src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+    printf ("Hello world from t1!\n");
+    return 0;
+}
---
+++ t3/lib/hello.h
@@ -0,0 +1,5 @@
+#ifndef HELLO_H_
+#define HELLO_H_
+#include <stdio.h>
+void HelloFunc();
+#endif
